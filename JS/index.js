6fdadfada5dd93abab3ea010755691ad7d37f55c
Point-Break22 <crystalsjgayle@gmail.com>
2022-08-27 09:13:59 -0400

    ......

--- JS/index.js
+++ JS/index.js
@@ -14,17 +14,6 @@
         });
     </script>
 
-    
-
-@media min width (whatever mobile size is) --- anything below 600 is display: none; 
-display:; block;
-
-max width 600, display none
-
-
-nav-link
-
-
 
 
 
